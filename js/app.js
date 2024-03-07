0c476c7af4ad3b41892ba39eb87006733094849f
MattiaBologna <mattiabologna01@gmail.com>
2024-03-07 18:00:54 +0100

    Add: rolling dices game

--- js/app.js
+++ js/app.js
@@ -24,12 +24,53 @@ mailSumbitDOMElement.addEventListener('click', function() {
 
         if (userMail === validUsersMail) {
             // mostra a schermo il messaggio per la mail valida 
-            const mailMessage = document.createElement('p')
-            mailMessage.innerHTML = 'The mail is valid'
-            const mailSectionDOMElement = document.querySelector('.mail')
-            mailSectionDOMElement.append(mailMessage)
+            const mailMessage = document.createElement('p');
+            mailMessage.innerHTML = 'The mail is valid';
+            const mailSectionDOMElement = document.querySelector('.mail');
+            mailSectionDOMElement.append(mailMessage);
         } 
+        // TODO aggiungere messaggio per mail non valida 
     } 
-    
+})
+
+
+// GIOCO DEI DADI
+
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+// recupera l'elemento del sumbit tira il dado per aggiungerci l'event listener 
+const diceRollerDOMElement = document.getElementById('dice--roller');
+
+diceRollerDOMElement.addEventListener('click', function() {
+    // recupera gli elementi del tiro dello user e dell'avversario 
+    const userRollDOMElement = document.getElementById('user--roll')
+    const computerrRollDOMElement = document.getElementById('computer--roll')
+
+    // genera un numero da 1 a sei da assegnare al tiro dello user
+    const userDiceRoll = Math.floor(Math.random() * 6) + 1; 
+    // genera un numero da 1 a sei da assegnare al tiro del computer
+    const computerDiceRoll = Math.floor(Math.random() * 6) + 1; 
+
+    // mostra il tiro dello user a schermo
+    userRollDOMElement.innerHTML = userDiceRoll
+
+    // mostra il tiro dell'avversario a schermo
+    computerrRollDOMElement.innerHTML = computerDiceRoll
+
+    // recupera la section dei dadi per appendere il risultato 
+    const dicesSectionDOMElement = document.querySelector('.dices')
+    const result = document.createElement('p')
+    dicesSectionDOMElement.append(result)
+
+    if (userDiceRoll === computerDiceRoll) {
+        result.innerHTML = "Pareggio"
+    } else if (userDiceRoll > computerDiceRoll) {
+        result.innerHTML = "Vittoria"
+    } else {
+        result.innerHTML = "Sconfitta"
+    }
+
     
 })
